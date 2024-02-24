f276016923788012faa0b887859ca6800a532e68
Richar Cangui <richarsantiagocangui96@gmail.com>
2024-02-24 01:38:57 -0500

    update: railway impl

--- src/main/java/com/richarcangui/store/operator/OperatorApplication.java
+++ src/main/java/com/richarcangui/store/operator/OperatorApplication.java
@@ -1,5 +1,6 @@
 package com.richarcangui.store.operator;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.loadbalancer.LoadBalanced;
@@ -8,6 +9,7 @@ import org.springframework.web.client.RestTemplate;
 
 
 @SpringBootApplication
+@Slf4j
 public class OperatorApplication {
 
 
@@ -18,6 +20,16 @@ public class OperatorApplication {
 	}
 
 	public static void main(String[] args) {
+		// Retrieve execution profile from environment variable. If not present, default profile is selected.
+		String profile = System.getenv("PROFILE");
+		System.setProperty("spring.profiles.active", profile != null ? profile : "default");
+		// Railway's internal interface takes some time to start. We wait for it to be ready.
+		log.debug("Waiting fo Internal Interface to start...");
+		try {
+			Thread.sleep(2000);
+		} catch (InterruptedException e) {
+			Thread.currentThread().interrupt();
+		}
 		SpringApplication.run(OperatorApplication.class, args);
 	}
 
